d95d47c1cb41540e18b5257996a813bb046eb62f
sivammohapatra1310 <121796805+sivammohapatra1310@users.noreply.github.com>
2025-03-13 20:50:22 +0000

    installing packages for backend

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
-import HomePage from './pages/HomePage';
+// import HomePage from './pages/HomePage';
 
 function App() {
   console.log('App component rendered');
